f4567202136cb49aeceabdd32a3f107e2e0e9300
its-kayes <kayes.ek8@gmail.com>
2022-06-11 12:50:56 +0600

    Get data to mongodb

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 let app = express();
+const port = process.env.PORT || 5000;
 
 
 app.use(cors());
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dkbab.mongodb.net/?retryWrites=true&w=majority`;
 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
         await client.connect();
-        let projectsCollection = client.db("portfolio").collection("projects");
+        let hotelCollection = client.db("hotels_details").collection("all_hotels");
 
-        app.get('/projects', async (req, res) => {
+        app.get('/hotels', async (req, res) => {
             let query = {}
             // let data = await projectsCollection.find(query).toArray();
-            let result = await projectsCollection.find(query).toArray();
+            let result = await hotelCollection.find(query).toArray();
             // res.send('kaj kore');
             res.send(result);
         });
